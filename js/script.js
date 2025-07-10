3b555c6f8d0b3e999681373f72747862634317f8
omarseddati16 <omarseddati16@gmail.com>
2025-07-10 16:32:23 +0200

    Calcolo

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
 
-const price = 0.21;
-const km = 100;
-const age = 10;
-const discount = 20;
-let baseprice = price * km;
-let finalprice = baseprice
-if (age < 18) {
-  const discountapplied = baseprice * (discount / 100);
-  finalprice = baseprice - discountapplied;
-}
-console.log(finalprice.toFixed(2))
+// bottone
+const button = document.getElementById('Biglietto');
 
-const ageoldman = 70;
-const discountold = 40;
-if (ageoldman >= 65) {
-  const discountapplied = baseprice * (discountold / 100)
-  finalprice = baseprice - discountapplied
-}
-console.log(finalprice.toFixed(2))
+// campi
+button.addEventListener('click', function (e) {
+  e.preventDefault();
 
+  const name = document.getElementById('identita').value
+  const km = document.getElementById('km').value
+  const eta = document.getElementById('age').value
+
+  console.log(name)
+  console.log(eta)
+  console.log(km)
+  // Calcolo
+  const price = 0.21;
+  const mintordiscount = 20;
+  const olderdiscountold = 40;
+  let baseprice = price * km;
+  let finalprice = baseprice
+  if (eta === 'Minorenne') {
+    const discountapplied = baseprice * (mintordiscount / 100);
+    finalprice = baseprice - discountapplied;
+  }
+  else if (eta === 'Maggiorenne') {
+    const discountapplied = baseprice * (olderdiscountold / 100);
+    finalprice = baseprice - discountapplied;
+  }
+  console.log(finalprice.toFixed(2))
+  document.getElementById('').innerText =
+    document.getElementById('').innerText =
+    document.getElementById('').innerText =
+
+})
