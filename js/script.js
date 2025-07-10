5a786e265ae8eb933b914d0579927c8fd9c9c1a9
omarseddati16 <omarseddati16@gmail.com>
2025-07-10 18:01:49 +0200

    fine parte java

--- js/script.js
+++ js/script.js
@@ -22,14 +22,24 @@ button.addEventListener('click', function (e) {
   if (eta === 'Minorenne') {
     const discountapplied = baseprice * (mintordiscount / 100);
     finalprice = baseprice - discountapplied;
+    ticketType = "Biglietto Minorenne";
   }
   else if (eta === 'Maggiorenne') {
     const discountapplied = baseprice * (olderdiscountold / 100);
     finalprice = baseprice - discountapplied;
+    ticketType = "Biglietto Maggiorenne";
   }
   console.log(finalprice.toFixed(2))
-  document.getElementById('').innerText =
-    document.getElementById('').innerText =
-    document.getElementById('').innerText =
+  // vagone
+  const vagone = Math.floor(Math.random() * 10) + 1;
+  const codice = Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000;
 
+
+  document.getElementById('showName').innerText = name;
+  document.getElementById('carriage').innerText = vagone;
+  document.getElementById('cpCode').innerText = codice;
+  document.getElementById('finalPrice').innerText = `${finalprice.toFixed(2)}`;
+
+  const biglietto = document.getElementById('tipoBiglietto');
+  biglietto.innerText = ticketType;
 })
